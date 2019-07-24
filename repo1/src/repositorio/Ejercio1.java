3a6ce40324652719b3150d6cc88cf15cfd948cc4
jonathanallasi <jonathanallasig@gmail.com>
2019-07-23 21:07:17 -0500

    ejercicio 1
    subido

--- repo1/src/repositorio/Ejercio1.java
+++ repo1/src/repositorio/Ejercio1.java
@@ -1,10 +1,30 @@
 package repositorio;
 
+import java.util.Scanner;
+
 public class Ejercio1 {
 
 	public Ejercio1() {
-		
+
 		// TODO Auto-generated constructor stub
+		Scanner teclado = new Scanner(System.in);
+		
+		int valor = 0;
+		int sum=0;
+		
+		do {
+			System.out.print("Ingrese numero: ");
+			int valor1 = teclado.nextInt();
+			valor =  valor1;
+			if(valor1>0) {
+				sum=valor+sum;
+			}
+
+		}while (valor > 0) ;
+		
+		
+		System.out.println("La suma de los numeros positivos es: "+sum);
+
 	}
 
 }
