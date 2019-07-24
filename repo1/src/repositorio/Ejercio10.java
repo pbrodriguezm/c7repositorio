abdcab6806bcc34f39995fb5a925717039274be6
clautvald <ctalaveravaldivia@gmail.com>
2019-07-23 21:54:59 -0500

    Ejercicio 6 - Practica 2

--- repo1/src/repositorio/Ejercio10.java
+++ repo1/src/repositorio/Ejercio10.java
@@ -1,9 +1,45 @@
 package repositorio;
 
+import java.util.Scanner;
+
 public class Ejercio10 {
 	public Ejercio10() {
-		
-		// TODO Auto-generated constructor stub
-	}
 
+		// TODO Auto-generated constructor stub
+
+		Scanner teclado = new Scanner(System.in);
+
+		System.out.println("Ingrese un valor límite positivo");
+
+		double numlimite = teclado.nextDouble();
+
+		while (numlimite <= 0) {
+			System.out.println("El valor debe ser mayor a 0. Ingrese un nuevo valor");
+			numlimite = teclado.nextDouble();
+		}
+
+		System.out.println("Ingrese un número");
+		double num1 = teclado.nextDouble();
+		if (num1 > numlimite) {
+			System.out.println("El número ingresado ha superado al número limite. Programa terminado");
+		} else {
+
+			System.out.println("Ingrese otro número");
+			double num2 = teclado.nextDouble();
+			
+			double suma = num1 + num2;
+			
+			while (suma < numlimite) {
+
+				System.out.println("Ingrese otro número");
+				 double num3 = teclado.nextDouble();
+				 
+				suma += num3;
+			}
+
+			System.out.println("La suma es :" + suma);
+			System.out.println("El numero ah superado el limite");
+		}
+
+	}
 }
