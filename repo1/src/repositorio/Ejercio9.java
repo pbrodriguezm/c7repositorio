3a74be4afb64db5b63e3a29128bbf9c981b4279e
Diego <diegotz20@hotmail.com>
2019-07-23 21:01:52 -0500

    Ejercicio 9 mejorado

--- repo1/src/repositorio/Ejercio9.java
+++ repo1/src/repositorio/Ejercio9.java
@@ -6,42 +6,27 @@ public class Ejercio9 {
 
 	public Ejercio9() {
 		// TODO Auto-generated constructor stub
-	
+
 		Scanner teclado = new Scanner(System.in);
-		
-		System.out.println("Ingresar 3 numeros enteros para el histograma");
-		
-		System.out.print("Ingrese el primer valor : ");
-		int a = teclado.nextInt();
-		
-		System.out.print("Ingrese el segundo valor: ");
-		int b = teclado.nextInt();
-		
-		System.out.print("Ingrese el tercer valor : ");
-		int c = teclado.nextInt();
-		
-		System.out.println(" ");
-		System.out.println("Los valores ingresados: ");
-		System.out.println(" ");
-		System.out.println("["+a+" "+b+" "+c+"]");
-		System.out.println(" ");
-		System.out.println("El histograma es: ");
-		System.out.println(" ");
-		for(int i = 0; i < a; i++) {
-			System.out.print("*");
+
+		System.out.println("De cuantos numeros sera el histograma");
+		int x = teclado.nextInt();
+		int[] num = new int[x];
+
+		System.out.println("Ingrese los " + x + " numeros para el histograma");
+
+		for (int i = 0; i < num.length; i++) {
+			System.out.print("Ingrese el " + (i + 1) + " numero: ");
+			num[i] = teclado.nextInt();
 		}
+
+		System.out.println(" ");
 		
-		System.out.print(" ");
-		
-		for(int i = 0; i < b; i++) {
-			System.out.print("*");
-		}
-		
-		System.out.print(" ");
-		
-		for(int i = 0; i < c; i++) {
-			System.out.print("*");
+		for (int e = 0; e < x; e++) {
+			for (int i = 0; i < num[e]; i++) {
+				System.out.print("*");
+			}
+			System.out.print(" ");
 		}
 	}
-
 }
