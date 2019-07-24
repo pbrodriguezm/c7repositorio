57f6b6a1e47d7498852591a9d2bc134a554fcbe6
jhonvc <52005655+jhonvc@users.noreply.github.com>
2019-07-23 22:07:58 -0500

    ejercicio3

--- repo1/src/repositorio/Ejercio3.java
+++ repo1/src/repositorio/Ejercio3.java
@@ -1,8 +1,30 @@
 package repositorio;
 
+import java.util.Scanner;
+
 public class Ejercio3 {
 	public Ejercio3() {
-		
+
 		// TODO Auto-generated constructor stub
+		int[] IngreseValor = new int[4];
+		IngreseValor[0] = 10;
+		IngreseValor[1] = 30;
+		IngreseValor[2] = 60;
+		IngreseValor[3] = 70;
+		
+		Scanner teclado= new Scanner(System.in);
+		System.out.println("Ingrese el maximo valor:");
+		int nm = teclado.nextInt();
+		int contador=0;
+		
+		for (int i = 0; i < IngreseValor.length; i++) {
+			if (IngreseValor[i] > nm) {
+				contador=contador+1;
+				System.out.println(IngreseValor[i] + "si es mayor");
+			}
+		}
+
+		System.out.println( "tenemos "+contador+" positivos");
+
 	}
 }
