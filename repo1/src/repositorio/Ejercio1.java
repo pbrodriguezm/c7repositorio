c99bd903c4a1828e88dbb13d71033b6d95056ed3
jonathanallasi <jonathanallasig@gmail.com>
2019-07-23 21:10:13 -0500

    EJERCICIO 1
    DESCRIPCION DE LO QUE REALIZA EL PROGRAMA

--- repo1/src/repositorio/Ejercio1.java
+++ repo1/src/repositorio/Ejercio1.java
@@ -6,7 +6,8 @@ public class Ejercio1 {
 
 	public Ejercio1() {
 
-		// TODO Auto-generated constructor stub
+		// INGRESAR NUMEROS HASTA QUE SE INGRESE UN NUMERO NEGATIVO Y ME MUESTRE 
+		//LA SUMA DE LOS NUMEROS POSITIVOS INGRESADOS
 		Scanner teclado = new Scanner(System.in);
 		
 		int valor = 0;
