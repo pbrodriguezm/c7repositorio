00a6549c7cafa582c0431326e1dd0e2130e651f9
Cordova9 <53122582+Cordova9@users.noreply.github.com>
2019-07-23 21:41:38 -0500

    ejercicio 6

--- repo1/src/repositorio/Ejercio6.java
+++ repo1/src/repositorio/Ejercio6.java
@@ -6,8 +6,7 @@ public class Ejercio6 {
 	public Ejercio6() {
 
 		// TODO Auto-generated constructor stub
-		int num1 = 0;
-		int num2 = 0;
+		
 		int var,valor,variab,variable;
 		Scanner teclado = new Scanner(System.in);
 		System.out.println("Escriba un numero");
@@ -16,7 +15,7 @@ public class Ejercio6 {
 		
 		System.out.println("Escriba un un numero mayor que " + valor);
 		variable = teclado.nextInt();
-		int resultado = (0);
+		
 		if (variable > valor) {
 			
 			System.out.println("Los numeros que ha escrito son " + variable + "," + valor);
