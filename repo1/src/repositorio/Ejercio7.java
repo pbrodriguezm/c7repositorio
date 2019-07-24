65486ecf1fceb53435bef94613d80ed44375b148
erickadelcarmendelacruzcalderondeocmin <53122737+erickadelcarmendelacruzcalderondeocmin@users.noreply.github.com>
2019-07-23 22:03:42 -0500

    Ejercicio7

--- repo1/src/repositorio/Ejercio7.java
+++ repo1/src/repositorio/Ejercio7.java
@@ -1,8 +1,17 @@
 package repositorio;
 
+import java.util.Scanner;
+
 public class Ejercio7 {
 	public Ejercio7() {
-		
-		// TODO Auto-generated constructor stub
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("Ingrese la palabra: ");
+		String valor = teclado.next(); // leer el teclado
+
+		char[] arrayvalor = valor.toCharArray(); // lo guarde en un array
+		for (int i = 0; i < valor.length(); i++) {
+
+			System.out.println(arrayvalor[i]);
+		}
 	}
 }
